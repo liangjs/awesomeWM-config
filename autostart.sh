441ba14b5016d547b500307c195509851829cdf7
liangjs <liangjs@pku.edu.cn>
2022-07-23 15:55:14 +0800

    init

---
+++ autostart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function run() {
+  if ! pgrep -f "$1" ;
+  then
+    "$@"&
+  fi
+}
+
+run numlockx on
+# run dunst
+run nm-applet
+run ibus-daemon -drx
+run volumeicon
+run light-locker --no-lock-on-lid
+run xautolock -time 25 -locker "light-locker-command -l"
+run picom -b
